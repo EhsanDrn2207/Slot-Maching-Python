50662dc055de5b18b348b1891e048213d7c59975
Ehsan Doroudian <ehsan.dorodian@gmail.com>
2025-01-26 15:54:52 +0330

    create pring_slot_machine function

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ def get_slot_machine_spin(rows, cols, symbols):
         for _ in range(symbol_count):
             all_symbols.append(symbol)
 
-    columns = [[], [], []]
-    
+    columns = []
     for _ in range(cols):
         column = []
         current_symbols = all_symbols[:]
@@ -33,6 +32,19 @@ def get_slot_machine_spin(rows, cols, symbols):
             column.append(value)
 
         columns.append(column)
+
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+
+        print()
+
 
 def deposit():
     while True:
@@ -94,8 +106,12 @@ def main():
         else:
             break
 
-        print(
-            f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}"
-        )
+    print(
+        f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}"
+    )
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+
 
 main()
