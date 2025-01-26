5779c51f9c477697b47cba95ec61da686774c63f
Ehsan Doroudian <ehsan.dorodian@gmail.com>
2025-01-26 15:17:52 +0330

    create get_bet function

--- main.py
+++ main.py
@@ -2,7 +2,23 @@ MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
 
+
 def deposit():
+    while True:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
+
+def get_number_of_lines():
     while True:
         lines = input("Enter the number of lines to bet on (1- " + str(MAX_LINES) + ")?")
         if lines.isdigit():
@@ -17,6 +33,38 @@ def deposit():
     return lines
 
 
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
 
-def get_number_of_lines():
-    
+    return amount
+
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(
+                f"You do not have enough to bet that amount, your current balance is: ${balance}"
+            )
+        else:
+            break
+
+        print(
+            f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}"
+        )
+
+main()
